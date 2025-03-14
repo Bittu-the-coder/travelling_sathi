368b4cac943ec7aaa2e49b7f4732a14c16874fbf
Bittu-the-coder <Bittuprajapati2271@gmail.com>
2025-03-14 19:51:33 +0530

    change

--- version-two/configs/FirebaseConfig.js
+++ version-two/configs/FirebaseConfig.js
@@ -6,13 +6,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8DMyMv0oCNztUHrT9e11mSROPlfEpjUo",
-  authDomain: "travelling-sathi.firebaseapp.com",
-  projectId: "travelling-sathi",
-  storageBucket: "travelling-sathi.firebasestorage.app",
-  messagingSenderId: "852272250087",
-  appId: "1:852272250087:web:a82e97e2bc6d36a7853642",
-  measurementId: "G-SCRXLYXEYG"
+
 };
 
 // Initialize Firebase
